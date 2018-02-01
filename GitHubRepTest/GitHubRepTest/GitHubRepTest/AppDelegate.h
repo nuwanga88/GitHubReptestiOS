330fea51199bee9db67b8a6c958d821ef13c5306
nuwanga88 <knuwangags@gmail.com>
2018-02-01 12:18:28 +1100

    First branch checkout

---
+++ GitHubRepTest/GitHubRepTest/GitHubRepTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubRepTest
+//
+//  Created by QA Software on 1/2/18.
+//  Copyright © 2018 NU HUB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubRepTest/GitHubRepTest/GitHubRepTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubRepTest
+//
+//  Created by QA Software on 1/2/18.
+//  Copyright © 2018 NU HUB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
